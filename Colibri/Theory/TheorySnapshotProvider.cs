94d168d90b524417ebb04449ce212ba281603ec1
Sergei <arragh1986@icloud.com>
2026-01-03 18:05:09 +0300

    chore: добавил пояснения к функции рекурсивного построения массива TempSegments.

--- Colibri/Theory/TheorySnapshotProvider.cs
+++ Colibri/Theory/TheorySnapshotProvider.cs
@@ -54,17 +54,33 @@ public class TheorySnapshotProvider
         TempSegment[] tempSegments,
         SegmentNode[] segmentsArray)
     {
+        /*
+         * Так как смысл кода неочевиден, добавил подробные комментарии,
+         * чтобы потом было проще вспомнить, что тут происходит.
+         */
+        
         int? firstChildIndex = null;
 
         for (int i = 0; i < segmentsArray.Length; i++)
         {
+            // Создаем модель и заполняем поля, которые сразу на 100% известны
             tempSegments[segmentIndex] = new TempSegment
             {
-                PathStartIndex = pathStartIndex,
-                ChildrenCount = segmentsArray[i].IncludedSegments.Count,
-                PathLength = segmentsArray[i].SegmentName.Length
+                PathStartIndex = pathStartIndex, // С какого индекса в массиве "paths" начинается имя текущего сегмента
+                PathLength = segmentsArray[i].SegmentName.Length, // Длина имени текущего сегмента в массиве "paths"
+                
+                /*
+                 * Количество наследников после сегмента.
+                 * Например, если имеем 2 маршрута "/users/{id}" и "/users/info",
+                 * то у сегмента "users" 2 наследника - "{id}" и "info"
+                 */
+                ChildrenCount = segmentsArray[i].IncludedSegments.Count
             };
 
+            /*
+             * Присваиваем индекс первого элемента в списке,
+             * чтобы вернуть его родителю как индекс первого наследника, т.е. FirstChildIndex
+             */
             if (firstChildIndex == null)
             {
                 firstChildIndex = segmentIndex;
@@ -72,24 +88,32 @@ public class TheorySnapshotProvider
             
             foreach (var c in segmentsArray[i].SegmentName)
             {
-                paths[pathStartIndex++] = c;
+                paths[pathStartIndex++] = c; // Заполняем общий массив "paths" символами имени сегмента
             }
             
-            segmentIndex++;
+            segmentIndex++; // Инкрементим индекс для следующего сегмента в цикле
         }
         
+        // Рекурсивный проход по наследникам
         for (int i = 0; i < segmentsArray.Length; i++)
         {
-            var temp = CreateTempSegmentsRecursively(
+            var frstChldIndx = CreateTempSegmentsRecursively(
                 ref pathStartIndex,
                 ref segmentIndex,
                 paths,
                 tempSegments,
                 segmentsArray[i].IncludedSegments.Values.ToArray());
 
-            if (temp != null)
+            // Если есть наследник и вернуло индекс, то присваиваем его в поле родителя
+            if (frstChldIndx != null)
             {
-                tempSegments[firstChildIndex!.Value + i].FirstChildIndex = temp.Value;
+                /*
+                 * Так как "segmentIndex" убежал вперед в предыдущем цикле,
+                 * то будем использовать "firstChildIndex", которому ранее было присвоено значение segmentIndex.
+                 * Так как нужно присвоить значения всем родителям, мы инкрементимся за счет итерации "i".
+                 * То есть это равносильно тому, что мы делали в предыдущем цикле, инкрементируя "segmentIndex"
+                 */
+                tempSegments[firstChildIndex!.Value + i].FirstChildIndex = frstChldIndx.Value;
             }
         }
 
