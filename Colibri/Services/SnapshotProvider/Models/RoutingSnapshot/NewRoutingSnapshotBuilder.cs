e0e422f6036129fd0272f11b65370379321f6dfd
Sergei <arragh1986@icloud.com>
2026-01-07 05:21:09 +0300

    feat: просто фиксация наработок

--- Colibri/Services/SnapshotProvider/Models/RoutingSnapshot/NewRoutingSnapshotBuilder.cs
+++ Colibri/Services/SnapshotProvider/Models/RoutingSnapshot/NewRoutingSnapshotBuilder.cs
@@ -1,17 +1,18 @@
 using Colibri.Configuration;
+using Colibri.Helpers;
 
 namespace Colibri.Services.SnapshotProvider.Models.RoutingSnapshot;
 
 public class NewRoutingSnapshotBuilder
 {
-    private NewTrieSegment _root = new();
-    private List<string> _hosts = new();
-    
     public void Build(RoutingSettings settings)
     {
-        var clusters = PrepareDataForTrie(settings);
-        FillTrie(clusters);
-        SortTrieRecursively(_root);
+        var clustersData = PrepareDataForTrie(settings);
+        var hosts = CreateHosts(clustersData);
+        var trie = CreateTrie(clustersData, hosts);
+        SortTrieRecursively(trie);
+
+        var lol = CreateTempSegments(trie);
 
         Console.WriteLine();
     }
@@ -38,26 +39,40 @@ public class NewRoutingSnapshotBuilder
         return clusters;
     }
 
-    private void FillTrie(List<NewTempCluster> clusters)
+    private List<string> CreateHosts(List<NewTempCluster> clustersData)
     {
-        foreach (var cluster in clusters)
+        var hosts = new List<string>();
+        
+        foreach (var cluster in clustersData)
         {
-            _hosts.AddRange(cluster.Hosts);
-            
+            hosts.AddRange(cluster.Hosts);
+        }
+        
+        return hosts;
+    }
+    
+    private NewTrieNode CreateTrie(List<NewTempCluster> clustersData, List<string> hosts)
+    {
+        var root = new NewTrieNode();
+        
+        foreach (var cluster in clustersData)
+        {
             foreach (var route in cluster.Routes)
             {
-                fillTrieRecursively(route, _root, cluster.Hosts);
+                fillTrieRecursively(route, root, cluster.Hosts);
             }
         }
         
-        void fillTrieRecursively(NewTempRoute route, NewTrieSegment root, string[] clusterHosts)
+        return root;
+        
+        void fillTrieRecursively(NewTempRoute route, NewTrieNode root, string[] clusterHosts)
         {
             var segment = root.ChildrenSegments
                 .FirstOrDefault(t => t.SegmentName == route.Upstream[0]);
         
             if (segment == null)
             {
-                segment = new NewTrieSegment
+                segment = new NewTrieNode
                 {
                     SegmentName = route.Upstream[0]
                 };
@@ -76,9 +91,9 @@ public class NewRoutingSnapshotBuilder
             if (route.Upstream.Length == 0)
             {
                 var indexes = new List<int>();
-                for (int i = 0; i < _hosts.Count; i++)
+                for (int i = 0; i < hosts.Count; i++)
                 {
-                    if (clusterHosts.Contains(_hosts[i]))
+                    if (clusterHosts.Contains(hosts[i]))
                     {
                         indexes.Add(i);
                     }
@@ -99,7 +114,7 @@ public class NewRoutingSnapshotBuilder
         }
     }
     
-    private void SortTrieRecursively(NewTrieSegment segment)
+    private void SortTrieRecursively(NewTrieNode segment)
     {
         segment.ChildrenSegments = segment.ChildrenSegments
             .OrderBy(s => s.IsParameter)
@@ -110,4 +125,76 @@ public class NewRoutingSnapshotBuilder
             SortTrieRecursively(child);
         }
     }
+
+    private (List<TempSegment>, List<TempDownstream>) CreateTempSegments(NewTrieNode root)
+    {
+        var tempSegments = new List<TempSegment>();
+        string segmentNames = string.Empty;
+        
+        var tempDownstreams = new List<TempDownstream>();
+        string downstreamPaths = string.Empty;
+
+        createTempSegmentsRecursively(
+            tempSegments,
+            tempDownstreams,
+            root,
+            ref segmentNames,
+            ref downstreamPaths);
+        
+        return (tempSegments, tempDownstreams);
+        
+        void createTempSegmentsRecursively(
+            List<TempSegment> tempSegments,
+            List<TempDownstream> tempDownstreams,
+            NewTrieNode node,
+            ref string segmentNames,
+            ref string downstreamPaths)
+        {
+            var tempSegment = new TempSegment();
+
+            if (node.SegmentName != null)
+            {
+                tempSegment.PathStartIndex = segmentNames.Length;
+                tempSegment.PathLength = (short)node.SegmentName.Length;
+                segmentNames += '/' + node.SegmentName;
+                
+                tempSegments.Add(tempSegment);
+                
+                if (node.ChildrenSegments.Count > 0)
+                {
+                    tempSegment.FirstChildIndex = tempSegments.Count;
+                    tempSegment.ChildrenCount = (short)node.ChildrenSegments.Count;
+                }
+                
+                foreach (var method in  node.Methods)
+                {
+                    var tempDownstream = new TempDownstream();
+                
+                    tempDownstream.PathStartIndex = downstreamPaths.Length;
+                    tempDownstream.PathLength = (short)method.Value.Length;
+                    downstreamPaths += method.Value;
+                    tempDownstream.MethodMask = HttpMethodMask.GetMask(method.Key);
+                    tempDownstream.HostStartIndex = (short)node.HostStartIndex;
+                    tempDownstream.HostsCount = (byte)node.HostsCount;
+                    
+                    tempDownstreams.Add(tempDownstream);
+
+                    tempSegment.DownstreamStartIndex = (short)tempDownstreams.Count;
+                    tempSegment.DownstreamCount++;
+                    tempSegment.MethodMask |= HttpMethodMask.GetMask(method.Key);
+                }
+            }
+
+            foreach (var child in node.ChildrenSegments)
+            {
+                createTempSegmentsRecursively(
+                    tempSegments,
+                    tempDownstreams,
+                    child,
+                    ref segmentNames,
+                    ref downstreamPaths);
+            }
+        }
+    }
+    
 }
