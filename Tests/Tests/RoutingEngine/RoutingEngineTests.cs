1e5bde906c3577866fb352d04d3e66abb0055a4e
Sergei <arragh1986@icloud.com>
2026-01-08 16:15:46 +0300

    feat: доделал тесты для RoutingEngine (наверное)

--- Tests/Tests/RoutingEngine/RoutingEngineTests.cs
+++ Tests/Tests/RoutingEngine/RoutingEngineTests.cs
@@ -1,17 +1,22 @@
+using Colibri.Configuration;
+using Colibri.Configuration.Models;
 using Colibri.Helpers;
 using Colibri.Snapshots.RoutingSnapshot;
-using Tests.Helpers;
 
 namespace Tests.Tests.RoutingEngine;
 
+/*
+ * Так как в данных тестах проверяется точное совпадение маршрутов и методов,
+ * то вместо общего глобального конфига используется локальный.
+ */
 public class RoutingEngineTests
 {
     [Fact]
-    public void TryMatch_InvalidPath_ResultIsFalse_ReturnsNull()
+    public void TryMatch_PathIsInvalid_ResultIsFalse_ReturnsNull()
     {
         // Arrange
         var routingSnapshotBuilder = new RoutingSnapshotBuilder();
-        var settings = RoutingSettingsHelper.MultipleClusters_MultipleRoutes();
+        var settings = GetLocalSettings();
         
         // Act
         var routingSnapshot = routingSnapshotBuilder.Build(settings);
@@ -31,11 +36,36 @@ public class RoutingEngineTests
     }
     
     [Fact]
-    public void TryMatch_ValidPath_ResultIsTrue_ReturnsNotNull()
+    public void TryMatch_PathIsValid_MethodIsInvalid_ResultIsFalse_ReturnsNull()
     {
         // Arrange
         var routingSnapshotBuilder = new RoutingSnapshotBuilder();
-        var settings = RoutingSettingsHelper.MultipleClusters_MultipleRoutes();
+        var settings = GetLocalSettings();
+        
+        // Act
+        var routingSnapshot = routingSnapshotBuilder.Build(settings);
+        var routingEngine = new Colibri.Services.RoutingEngine.RoutingEngine();
+        
+        var requestPath = "/tests/users".AsSpan();
+        var methodMask = HttpMethodMask.GetMask("GET");
+        
+        var matchResult = routingEngine.TryMatch(
+            routingSnapshot,
+            requestPath,
+            methodMask,
+            out var downstream);
+        
+        // Assert
+        Assert.False(matchResult);
+        Assert.Null(downstream);
+    }
+    
+    [Fact]
+    public void TryMatch_PathIsValid_ResultIsTrue_ReturnsNotNull()
+    {
+        // Arrange
+        var routingSnapshotBuilder = new RoutingSnapshotBuilder();
+        var settings = GetLocalSettings();
         
         // Act
         var routingSnapshot = routingSnapshotBuilder.Build(settings);
@@ -64,5 +94,43 @@ public class RoutingEngineTests
         Assert.True(matchResult2);
         Assert.NotNull(downstream1);
         Assert.NotNull(downstream2);
+    }
+    
+    private RoutingSettings GetLocalSettings()
+    {
+        return new RoutingSettings
+        {
+            Clusters = [
+                new ClusterDto
+                {
+                    Protocol = "Http",
+                    Hosts = [
+                        "http://escapepod:5000",
+                        "http://escapepod:5001"
+                    ],
+                    Routes =
+                    [
+                        new RouteDto
+                        {
+                            Method = "POST",
+                            UpstreamPattern = "/tests/users",
+                            DownstreamPattern = "/internal/users"
+                        },
+                        new RouteDto
+                        {
+                            Method = "GET",
+                            UpstreamPattern = "/tests/users/{name}/info",
+                            DownstreamPattern = "/profile/{login}/info"
+                        },
+                        new RouteDto
+                        {
+                            Method = "GET",
+                            UpstreamPattern = "/tests/users/me",
+                            DownstreamPattern = "/api/clients"
+                        }
+                    ]
+                },
+            ]
+        };
     }
 }
