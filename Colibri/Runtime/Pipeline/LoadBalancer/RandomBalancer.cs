a011df390f2c427e6d7c279fd7dc95af5bd7fca7
Sergei <arragh1986@icloud.com>
2026-01-31 21:25:11 +0300

    feat: черновая реализация RND-LB.

--- Colibri/Runtime/Pipeline/LoadBalancer/RandomBalancer.cs
+++ Colibri/Runtime/Pipeline/LoadBalancer/RandomBalancer.cs
@@ -6,6 +6,17 @@ public sealed class RandomBalancer(HostState[] hostStates) : ILoadBalancer
     
     public int SelectHost()
     {
+        // TODO: В идеале тут надо разделить Degraded и Healthy-хосты и работать только по Healthy, но для MVP и так сойдет.
+        for (int i = 0; i < _hostsCount; i++)
+        {
+            var hostIdx = Random.Shared.Next(_hostsCount);
+
+            if (Volatile.Read(ref hostStates[hostIdx].State) != 1)
+            {
+                return hostIdx;
+            }
+        }
+        
         return Random.Shared.Next(_hostsCount);
     }
 }
