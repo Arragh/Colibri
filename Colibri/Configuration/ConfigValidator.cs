24d066cb27d62e8cba1c5adf871898e207cbce44
Sergei <arragh1986@icloud.com>
2026-02-21 02:37:59 +0300

    feat: updating routes validation

--- Colibri/Configuration/ConfigValidator.cs
+++ Colibri/Configuration/ConfigValidator.cs
@@ -5,6 +5,18 @@ namespace Colibri.Configuration;
 
 public static class ConfigValidator
 {
+    private static readonly HashSet<string> ValidHttpMethods =
+    [
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+        "HEAD",
+        "OPTIONS",
+        "TRACE"
+    ];
+
     public static bool Validate(ColibriSettings settings)
     {
         var errors = new List<string>();
@@ -29,7 +41,10 @@ public static class ConfigValidator
         {
             ValidateStaticSegments(route, errors);
             ValidateParams(route, errors);
+            ValidateMethods(route, errors);
         }
+        
+        CheckForDuplicates(routes, errors);
 
         if (errors.Count > 0)
         {
@@ -38,88 +53,134 @@ public static class ConfigValidator
                 typeof(RoutingSettings),
                 errors.ToArray());
         }
-    }
 
-    private static void ValidateStaticSegments(RouteCfg route, List<string> errors)
-    {
-        var upstreamSegments = route.UpstreamPattern
-            .Split('/', StringSplitOptions.RemoveEmptyEntries)
-            .Where(s => !s.StartsWith('{') && !s.EndsWith('}'));
-        
-        var downstreamSegments = route.DownstreamPattern
-            .Split('/', StringSplitOptions.RemoveEmptyEntries)
-            .Where(s => !s.StartsWith('{') && !s.EndsWith('}'));
+        return;
 
-        foreach (var segment in upstreamSegments)
+        void ValidateStaticSegments(RouteCfg route, List<string> errors)
         {
-            if (segment.Length > 250)
+            var upstreamSegments = route.UpstreamPattern
+                .Split('/', StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => !s.StartsWith('{') && !s.EndsWith('}'));
+            
+            var downstreamSegments = route.DownstreamPattern
+                .Split('/', StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => !s.StartsWith('{') && !s.EndsWith('}'));
+
+            foreach (var segment in upstreamSegments)
             {
-                errors.Add($"Segment '{segment}' in route {route.UpstreamPattern} is longer than 250 characters");
-            }
-        }
-        
-        foreach (var segment in downstreamSegments)
-        {
-            if (segment.Length > 250)
-            {
-                errors.Add($"Segment '{segment}' in route {route.DownstreamPattern} is longer than 250 characters");
-            }
-        }
-    }
-
-    private static void ValidateParams(RouteCfg route, List<string> errors)
-    {
-        var upstreamSegments = route.UpstreamPattern
-            .Split('/', StringSplitOptions.RemoveEmptyEntries);
-        
-        var downstreamSegments = route.DownstreamPattern
-            .Split('/', StringSplitOptions.RemoveEmptyEntries);
-
-        for (int i = 0; i < upstreamSegments.Length; ++i)
-        {
-            if ((upstreamSegments[i].StartsWith('{') && !upstreamSegments[i].EndsWith('}'))
-                || (upstreamSegments[i].EndsWith('}') && !upstreamSegments[i].StartsWith('{')))
-            {
-                errors.Add($"'{upstreamSegments[i]}' is not a valid route segment in '{route.UpstreamPattern}'");
-            }
-        }
-        
-        var upstreamParams = upstreamSegments
-            .Where(s => s.StartsWith('{') && s.EndsWith('}'))
-            .ToArray();
-        
-        var downstreamParams = downstreamSegments
-            .Where(s => s.StartsWith('{') && s.EndsWith('}'))
-            .ToArray();
-        
-        if (upstreamParams.Length > 16)
-        {
-            errors.Add($"Too many parameters ({upstreamParams.Length}) in route '{route.UpstreamPattern}'");
-        }
-
-        if (downstreamParams.Length > 16)
-        {
-            errors.Add($"Too many parameters ({downstreamParams.Length}) in route '{route.DownstreamPattern}'");
-        }
-
-        if (upstreamParams.Length != downstreamParams.Length)
-        {
-            errors.Add($"Different number of parameters in route {route.UpstreamPattern}");
-        }
-
-        for (int i = 0; i < upstreamParams.Length; ++i)
-        {
-            for (int j = i + 1; j < upstreamParams.Length; ++j)
-            {
-                if (upstreamParams[i] == upstreamParams[j])
+                if (segment.Length > 250)
                 {
-                    errors.Add($"Duplicate parameter {upstreamParams[i]} in route {route.UpstreamPattern}");
+                    errors.Add($"Segment '{segment}' in upstream {route.UpstreamPattern} is longer than 250 characters");
                 }
-
-                if (downstreamParams[i] == downstreamParams[j])
+            }
+            
+            foreach (var segment in downstreamSegments)
+            {
+                if (segment.Length > 250)
                 {
-                    Console.WriteLine($"Duplicate parameter {downstreamParams[i]} in route {route.DownstreamPattern}");
-                    errors.Add($"Duplicate parameter {downstreamParams[i]} in route {route.DownstreamPattern}");
+                    errors.Add($"Segment '{segment}' in downstream {route.DownstreamPattern} is longer than 250 characters");
+                }
+            }
+        }
+
+        void ValidateParams(RouteCfg route, List<string> errors)
+        {
+            var upstreamSegments = route.UpstreamPattern
+                .Split('/', StringSplitOptions.RemoveEmptyEntries);
+            
+            var downstreamSegments = route.DownstreamPattern
+                .Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < upstreamSegments.Length; ++i)
+            {
+                if ((upstreamSegments[i].StartsWith('{') && !upstreamSegments[i].EndsWith('}'))
+                    || (upstreamSegments[i].EndsWith('}') && !upstreamSegments[i].StartsWith('{')))
+                {
+                    errors.Add($"'{upstreamSegments[i]}' is not a valid route segment in upstream '{route.UpstreamPattern}'");
+                }
+            }
+            
+            var upstreamParams = upstreamSegments
+                .Where(s => s.StartsWith('{') && s.EndsWith('}'))
+                .ToArray();
+            
+            var downstreamParams = downstreamSegments
+                .Where(s => s.StartsWith('{') && s.EndsWith('}'))
+                .ToArray();
+            
+            if (upstreamParams.Length > 16)
+            {
+                errors.Add($"Too many parameters ({upstreamParams.Length}) in upstream '{route.UpstreamPattern}'");
+            }
+
+            if (downstreamParams.Length > 16)
+            {
+                errors.Add($"Too many parameters ({downstreamParams.Length}) in downstream '{route.DownstreamPattern}'");
+            }
+
+            if (upstreamParams.Length != downstreamParams.Length)
+            {
+                errors.Add($"Different number of parameters in upstream {route.UpstreamPattern}");
+            }
+
+            for (int i = 0; i < upstreamParams.Length; ++i)
+            {
+                for (int j = i + 1; j < upstreamParams.Length; ++j)
+                {
+                    if (upstreamParams[i] == upstreamParams[j])
+                    {
+                        errors.Add($"Duplicate parameter {upstreamParams[i]} in upstream {route.UpstreamPattern}");
+                    }
+
+                    if (downstreamParams[i] == downstreamParams[j])
+                    {
+                        errors.Add($"Duplicate parameter {downstreamParams[i]} in downstream {route.DownstreamPattern}");
+                    }
+                }
+            }
+        }
+
+        void ValidateMethods(RouteCfg route, List<string> errors)
+        {
+            foreach (var method in route.Methods)
+            {
+                if (!ValidHttpMethods.Contains(method))
+                {
+                    var upper = method.ToUpperInvariant();
+
+                    if (ValidHttpMethods.Contains(upper))
+                    {
+                        errors.Add($"HTTP method '{method}' in upstream '{route.UpstreamPattern}' must be uppercase");
+                    }
+                    else
+                    {
+                        errors.Add($"HTTP method '{method}' is not a valid method in upstream '{route.UpstreamPattern}'");
+                    }
+                }
+            }
+        }
+        
+        void CheckForDuplicates(RouteCfg[] routes, List<string> errors)
+        {
+            for (int i = 0; i < routes.Length; ++i)
+            {
+                var route1 = routes[i];
+
+                for (int j = i + 1; j < routes.Length; j++)
+                {
+                    var route2 = routes[j];
+                    
+                    if (route1.UpstreamPattern == route2.UpstreamPattern)
+                    {
+                        var duplicates = route1.Methods
+                            .Intersect(route2.Methods, StringComparer.OrdinalIgnoreCase)
+                            .ToArray();
+
+                        foreach (var method in duplicates)
+                        {
+                            errors.Add($"Duplicate method {method} in upstream {route1.UpstreamPattern}");
+                        }
+                    }
                 }
             }
         }
