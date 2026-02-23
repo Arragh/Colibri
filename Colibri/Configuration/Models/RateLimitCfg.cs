716f7af70ca70aee0633ead5e8be860fc1d8dfde
Sergei <arragh1986@icloud.com>
2026-02-23 03:07:21 +0300

    refactor: cleaned unused fields in ClusterCfg.cs

--- Colibri/Configuration/Models/RateLimitCfg.cs
+++
@@ -1,7 +0,0 @@
-namespace Colibri.Configuration.Models;
-
-public sealed class RateLimitCfg
-{
-    public bool Enabled { get; set; }
-    public int RequestsPerMinute { get; set; }
-}
--- Colibri/Configuration/Models/TimeoutCfg.cs
+++
@@ -1,7 +0,0 @@
-namespace Colibri.Configuration.Models;
-
-public sealed class TimeoutCfg
-{
-    public int Request { get; set; }
-    public int Connect { get; set; }
-}
