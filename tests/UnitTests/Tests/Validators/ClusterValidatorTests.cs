7fa58aab430158f115b9f751fe616115de0ece04
Sergei <arragh1986@icloud.com>
2026-02-26 04:21:42 +0300

    chore: now tests fixed

--- tests/UnitTests/Tests/Validators/ClusterValidatorTests.cs
+++ tests/UnitTests/Tests/Validators/ClusterValidatorTests.cs
@@ -299,6 +299,6 @@ public sealed class ClusterValidatorTests
         var result = _validator.LoadBalancerTypeIsValid(type);
         
         // Assert
-        Assert.False(result);
+        Assert.True(result);
     }
 }
